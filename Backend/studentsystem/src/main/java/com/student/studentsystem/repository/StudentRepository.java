efd97f8df8ecf529af2a62ba29b7148c42d7e249
NuwanthiJ <151948936+NuwanthiJ@users.noreply.github.com>
2025-02-04 21:20:26 +0530

    Create Repository Class

---
+++ Backend/studentsystem/src/main/java/com/student/studentsystem/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.student.studentsystem.repository;
+
+import com.student.studentsystem.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Integer> {
+}
